6194762d185a31f281b738607f9368c4959e019a
Thomas von Deyen <thomas@vondeyen.com>
2021-04-07 21:30:41 +0200

    Speed up vat price migration by eager loading

--- core/app/models/spree/variant/vat_price_generator.rb
+++ core/app/models/spree/variant/vat_price_generator.rb
@@ -14,26 +14,45 @@ module Spree
     #     - A price for any other country (country_id == nil): 8.40
     #
     class VatPriceGenerator
-      attr_reader :variant
+      attr_reader :variant, :verbose, :countries
 
-      def initialize(variant)
+      def initialize(variant, verbose: false, countries: Spree::Country.all)
         @variant = variant
+        @verbose = verbose
+        @countries = countries.to_a
       end
 
       def run
+        puts "\n= Migrating vat prices for #{variant.sku.presence || variant.name.presence || variant.id}" if verbose
+
         # Early return if there is no VAT rates in the current store.
-        return if !variant.tax_category || variant_vat_rates.empty?
+        if !variant.tax_category || variant_vat_rates.empty?
+          puts "• No tax category or tax rates found. Skipping" if verbose
+          return
+        end
 
         country_isos_requiring_price.each do |country_iso|
           # Don't re-create the default price
           next if variant.default_price && variant.default_price.country_iso == country_iso
 
-          foreign_price = variant.prices.find_or_initialize_by(
-            country_iso: country_iso,
-            currency: variant.default_price.currency,
-          )
+          if variant.default_price.nil?
+            puts "• No default price found. Skipping" if verbose
+            next
+          end
 
-          foreign_price.amount = variant.default_price.net_amount * (1 + vat_for_country_iso(country_iso))
+          currency = variant.default_price.currency
+          if variant.prices.any? { |p| p.country_iso == country_iso && p.currency == currency }
+            puts "• Already has price for #{country_iso || "all"} in #{currency}. Skipping" if verbose
+            next
+          end
+
+          amount = variant.default_price.net_amount * (1 + vat_for_country_iso(country_iso))
+          puts "√ Set price for #{country_iso || "all"} to #{currency} #{"%.2f" % amount.round(2)}" if verbose
+          variant.prices.build(
+            country_iso: country_iso,
+            currency: currency,
+            amount: amount,
+          )
         end
       end
 
@@ -42,16 +61,25 @@ module Spree
       # nil is added to the array so we always have an export price.
       def country_isos_requiring_price
         return [nil] unless variant.tax_category
+
         [nil] + variant_vat_rates.map(&:zone).flat_map(&:countries).flat_map(&:iso)
       end
 
       def vat_for_country_iso(country_iso)
-        return 0 unless variant.tax_category
-        variant_vat_rates.for_country(Spree::Country.find_by(iso: country_iso)).sum(:amount)
+        return 0.0 if !variant.tax_category || country_iso.nil?
+
+        country = countries.find { |c| c.iso == country_iso }
+        variant_vat_rates.select do |tax_rate|
+          tax_rate.zone.countries.any? do |zone_country|
+            zone_country.id == country.id
+          end
+        end.sum(&:amount)
       end
 
       def variant_vat_rates
-        @variant_vat_rates ||= variant.tax_category.tax_rates.where(included_in_price: true)
+        @variant_vat_rates ||= variant.tax_category.tax_rates.select(&:included_in_price).reject do |rate|
+          rate.amount.zero?
+        end
       end
     end
   end
--- core/lib/spree/core/price_migrator.rb
+++ core/lib/spree/core/price_migrator.rb
@@ -3,28 +3,37 @@ module Spree
   # the `solidus:migrations:create_vat_prices` rake task.
   class PriceMigrator
     # Migrate all variant's prices.
-    def self.migrate_default_vat_prices
-      # We need to tag the exisiting prices as "default", so that the VatPriceGenerator knows
-      # that they include the default zone's VAT.
-      Spree::Config.admin_vat_country_iso = Spree::Zone.default_tax.countries.first.iso
-      Spree::Variant.find_each do |variant|
-        new(variant).migrate_vat_prices
+    class << self
+      def migrate_default_vat_prices
+        # We need to tag the exisiting prices as "default", so that the VatPriceGenerator knows
+        # that they include the default zone's VAT.
+        Spree::Config.admin_vat_country_iso ||= Spree::Zone.default_tax.countries.first.iso
+        # With a default tax zone, all prices include VAT by default. Let's tell them which one!
+        Spree::Price.joins(:variant).update_all(country_iso: Spree::Config.admin_vat_country_iso)
+        countries = Spree::Country.all
+        Spree::Variant.includes(*variant_includes).find_each do |variant|
+          Spree::Variant::VatPriceGenerator.new(variant, verbose: !Rails.env.test?, countries: countries).run
+          variant.save
+        end
+        # This line stops all weird code paths that generate refunds from happening.
+        Spree::Zone.default_tax.update(default_tax: false)
       end
-      # This line stops all weird code paths that generate refunds from happening.
-      Spree::Zone.default_tax.update(default_tax: false)
-    end
 
-    attr_reader :variant
+      private
 
-    def initialize(variant)
-      @variant = variant
-    end
-
-    def migrate_vat_prices
-      # With a default tax zone, all prices include VAT by default. Let's tell them which one!
-      variant.prices.update_all(country_iso: Spree::Config.admin_vat_country_iso)
-      Spree::Variant::VatPriceGenerator.new(variant).run
-      variant.save
+      def variant_includes
+        [
+          :prices,
+          :default_price,
+          {
+            tax_category: {
+              tax_rates: {
+                zone: :countries,
+              },
+            },
+          },
+        ]
+      end
     end
   end
 end
